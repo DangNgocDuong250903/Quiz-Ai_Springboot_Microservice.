d96b5a10646065e2f34ede5d3da65449443d5be1
Đặng Ngọc Dương <94966949+DangNgocDuong250903@users.noreply.github.com>
2025-08-13 19:57:29 +0700

    Update MailConfig.java

--- BE_QUIZ/notification-service/src/main/java/com.LinkVerse/notification/configuration/MailConfig.java
+++ BE_QUIZ/notification-service/src/main/java/com.LinkVerse/notification/configuration/MailConfig.java
@@ -15,8 +15,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("dangngocduong2509@gmail.com");
-        mailSender.setPassword("qenz zvxi qzdy lupo");
+        mailSender.setUsername("xx@gmail.com");
+        mailSender.setPassword("x x x x");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
